55355be7f224e426714343ee2abe1f58e914372b
Luan3642 <luan3642@hotmail.com>
2017-05-28 23:00:23 -0300

    arrumar erro 404

---
+++ JSF/src/java/br/com/senai/bean/FornecedorBean.java
@@ -0,0 +1,56 @@
+package br.com.senai.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.senai.dao.FornecedorDAO;
+import br.com.senai.model.Fornecedor;
+
+@ManagedBean
+@SessionScoped
+public class FornecedorBean {
+
+	private Fornecedor fornecedor = new Fornecedor();
+	private List<Fornecedor> fornecedores = new ArrayList<>();
+	
+	public FornecedorBean() {
+		fornecedores = new FornecedorDAO().listar();
+	}
+	
+	public String salvar(){
+		//fornecedores.add(fornecedor);
+		
+		new FornecedorDAO().salvar(fornecedor);
+		fornecedores = new FornecedorDAO().listar();
+		fornecedor = new Fornecedor();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cidade salva com sucesso!"));
+		return "fornecedor_list?faces-redirect=true";
+	}
+	
+	public String editar(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+		return "fornecedor_cad?faces-redirect=true";
+	}
+
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	public List<Fornecedor> getFornecedores() {
+		return fornecedores;
+	}
+
+	public void setFornecedores(List<Fornecedor> fornecedores) {
+		this.fornecedores = fornecedores;
+	}
+
+}
